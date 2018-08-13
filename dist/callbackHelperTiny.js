c8aa7f57570ea4a132741e41d6642d420c36fe51
Ilya Pavlov <iluxz@mail.ru>
2018-08-13 23:23:51 +0300

    add callbackHelperTiny.js tsconfig.json tslint.json and etc..

--- dist/callbackHelperTiny.js
+++ dist/callbackHelperTiny.js
@@ -1,18 +1,15 @@
+"use strict";
 // callback-helper-tiny - Copyright (C) 2018 Ilya Pavlov
 // callback-helper-tiny is licensed under the MIT License
-
-export function clbWaitAll(
-    fns: (( clb: ( err: Error | null, data?: any ) => void ) => void)[],
-    clb: (err: Error | null, data?: any) => void
-): void {
-    let l: number = fns.length;
-    let n: number = 0;
-    let hasError: boolean = false;
-    let i: number;
-    let mixedResults: any[] = [];
-
-    for (i=0; i<l; i++) {
-        (fns[i])(function(err: Error | null, data?: any): void {
+Object.defineProperty(exports, "__esModule", { value: true });
+function clbWaitAll(fns, clb) {
+    var l = fns.length;
+    var n = 0;
+    var hasError = false;
+    var i;
+    var mixedResults = [];
+    for (i = 0; i < l; i++) {
+        (fns[i])(function (err, data) {
             if (false === hasError) {
                 if (err) {
                     hasError = true;
@@ -20,7 +17,6 @@ export function clbWaitAll(
                 }
                 else {
                     mixedResults[n] = data;
-
                     if (++n === l) {
                         clb(null, mixedResults);
                     }
@@ -29,24 +25,17 @@ export function clbWaitAll(
         });
     }
 }
-
-
-
-export function clbQueue(
-    fns: (( clb: ( err: Error | null, data?: any ) => void ) => void)[],
-    clb: (err: Error | null, data?: any) => void
-): void {
-    let l: number = fns.length;
-    let n: number = 0;
-    let results: any[] = [];
-
-    let next = function(err: Error | null, data?: any): void {
+exports.clbWaitAll = clbWaitAll;
+function clbQueue(fns, clb) {
+    var l = fns.length;
+    var n = 0;
+    var results = [];
+    var next = function (err, data) {
         if (err) {
             clb(err, data);
         }
         else {
             results[n] = data;
-
             if (++n === l) {
                 clb(null, results);
             }
@@ -55,6 +44,6 @@ export function clbQueue(
             }
         }
     };
-
     (fns[n])(next);
 }
+exports.clbQueue = clbQueue;
---
+++ test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var expect = require('chai').expect;
+var A = require('../dist/callbackHelperTiny.js');
+
+
+
