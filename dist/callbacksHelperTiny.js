6c5e27f490b0a54596091c908ec9d92a48a38145
Ilya Pavlov <iluxz@mail.ru>
2019-02-20 21:30:56 +0300

    generate dist files

--- dist/callbacksHelperTiny.js
+++ dist/callbacksHelperTiny.js
@@ -2,52 +2,76 @@
 // callbacks-helper-tiny - Copyright (C) 2018-2019 Ilya Pavlov
 // callbacks-helper-tiny is licensed under the MIT License
 Object.defineProperty(exports, "__esModule", { value: true });
-function clbWaitAll(fns, clb) {
+function cbWaitAll(fns, cb) {
     var l = fns.length;
     if (0 === l) {
-        clb(null, []);
+        cb(null);
         return;
     }
     var n = 0;
     var hasError = false;
     var i;
-    var mixedResults = [];
     for (i = 0; i < l; ++i) {
-        (fns[i])(function (err, data) {
+        (fns[i])(function (err) {
             if (false === hasError) {
                 if (err) {
                     hasError = true;
-                    clb(err, []);
+                    cb(err);
                 }
-                else {
-                    mixedResults[n] = data;
-                    if (++n === l) {
-                        clb(null, mixedResults);
-                    }
+                else if (++n === l) {
+                    cb(null);
                 }
             }
         });
     }
 }
-exports.clbWaitAll = clbWaitAll;
-function clbQueue(fns, clb) {
+exports.cbWaitAll = cbWaitAll;
+function cbWaitAllWithData(fns, cb) {
     var l = fns.length;
     if (0 === l) {
-        clb(null, []);
+        cb(null, []);
         return;
     }
-    var maxStackCalls = 256;
+    var n = 0;
+    var hasError = false;
+    var results = [];
+    var _loop_1 = function (i) {
+        (fns[i])(function (err, data) {
+            if (false === hasError) {
+                if (err) {
+                    hasError = true;
+                    cb(err, []);
+                }
+                else {
+                    results[i] = data;
+                    if (++n === l) {
+                        cb(null, results);
+                    }
+                }
+            }
+        });
+    };
+    for (var i = 0; i < l; ++i) {
+        _loop_1(i);
+    }
+}
+exports.cbWaitAllWithData = cbWaitAllWithData;
+function cbQueue(fns, cb, maxStackCalls) {
+    if (maxStackCalls === void 0) { maxStackCalls = 256; }
+    var l = fns.length;
+    if (0 === l) {
+        cb(null);
+        return;
+    }
     var stackCalls = maxStackCalls;
     var n = 0;
-    var results = [];
-    var next = function (err, data) {
+    var next = function (err) {
         if (err) {
-            clb(err, []);
+            cb(err);
         }
         else {
-            results[n] = data;
             if (++n === l) {
-                clb(null, results);
+                cb(null);
             }
             else {
                 if (0 === --stackCalls) {
@@ -61,5 +85,47 @@ function clbQueue(fns, clb) {
         }
     };
     (fns[n])(next);
+}
+exports.cbQueue = cbQueue;
+function cbQueueWithData(fns, cb, maxStackCalls) {
+    if (maxStackCalls === void 0) { maxStackCalls = 256; }
+    var l = fns.length;
+    if (0 === l) {
+        cb(null, []);
+        return;
+    }
+    var stackCalls = maxStackCalls;
+    var n = 0;
+    var results = [];
+    var next = function (err, data) {
+        if (err) {
+            cb(err, []);
+        }
+        else {
+            results[n] = data;
+            if (++n === l) {
+                cb(null, results);
+            }
+            else {
+                if (0 === --stackCalls) {
+                    stackCalls = maxStackCalls;
+                    setTimeout(function () { return (fns[n])(next); }, 0);
+                }
+                else {
+                    (fns[n])(next);
+                }
+            }
+        }
+    };
+    (fns[n])(next);
+}
+exports.cbQueueWithData = cbQueueWithData;
+// DEPRECATED
+function clbWaitAll(fns, clb) {
+    cbWaitAllWithData(fns, clb);
+}
+exports.clbWaitAll = clbWaitAll;
+function clbQueue(fns, clb) {
+    cbQueueWithData(fns, clb);
 }
 exports.clbQueue = clbQueue;
